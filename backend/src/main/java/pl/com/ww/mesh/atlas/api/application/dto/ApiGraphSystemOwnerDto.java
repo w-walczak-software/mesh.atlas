51082a9e867fc8c9a458ff8fb9248ca25d978527
Wojciech Walczak <w.walczak.software@gmail.com>
2026-06-10 17:28:25 +0200

    szczeg. systemu

---
+++ backend/src/main/java/pl/com/ww/mesh/atlas/api/application/dto/ApiGraphSystemOwnerDto.java
@@ -0,0 +1,8 @@
+package pl.com.ww.mesh.atlas.api.application.dto;
+
+public record ApiGraphSystemOwnerDto(
+        String firstName,
+        String lastName,
+        String email,
+        String roleName
+) {}
